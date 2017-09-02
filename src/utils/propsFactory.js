99a17390ba284dd08475b22be6e4379f10a4e69f
Nitin <tulswani19@gmail.com>
2017-09-02 15:38:56 +0530

    added new props

---
+++ src/utils/propsFactory.js
@@ -0,0 +1,93 @@
+/**
+ * Returns an object of image filter props
+ * @param { object } props Component props
+ */
+const filterPropsToListen = props => {
+	const {
+		resize,
+		quality,
+		greyscale,
+		contain,
+		cover,
+		normalize,
+		invert,
+		opaque,
+		sepia,
+		dither565,
+		scale,
+		scaleToFit,
+		flip,
+		rotate,
+		brightness,
+		contrast,
+		fade,
+		opacity,
+		blur,
+		posterize,
+		colors,
+	} = props;
+
+	return {
+		resize,
+		quality,
+		greyscale,
+		contain,
+		cover,
+		normalize,
+		invert,
+		opaque,
+		sepia,
+		dither565,
+		scale,
+		scaleToFit,
+		flip,
+		rotate,
+		brightness,
+		contrast,
+		fade,
+		opacity,
+		blur,
+		posterize,
+		colors,
+	};
+};
+
+/**
+ * Returns the rest of the props of the image component like className, alt
+ * @param { object } props Component props
+ */
+const getImageProps = props => {
+	const {
+		image,
+		resize,
+		quality,
+		greyscale,
+		contain,
+		cover,
+		normalize,
+		invert,
+		opaque,
+		sepia,
+		dither565,
+		scale,
+		scaleToFit,
+		flip,
+		rotate,
+		brightness,
+		contrast,
+		fade,
+		opacity,
+		blur,
+		posterize,
+		colors,
+		placeholder,
+		processedImage,
+		storage,
+		disableWebWorker,
+		...rest
+	} = props;
+
+	return rest;
+};
+
+export { filterPropsToListen, getImageProps };
