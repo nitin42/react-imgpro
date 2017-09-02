157e510b96d7726746e4e1dada5408562c79fa56
Nitin <tulswani19@gmail.com>
2017-09-02 15:46:36 +0530

    use spaces

--- src/worker.js
+++ src/worker.js
@@ -3,7 +3,7 @@ const processImage = require('./utils/options');
 // Code below is executed in web worker (above code is not intensive and is processed in main thread and not in web worker)
 module.exports = function worker(self) {
 	self.onmessage = function(e) {
-		importScripts('https://cdn.rawgit.com/oliver-moran/jimp/v0.2.27/browser/lib/jimp.min.js');	
+		importScripts('https://cdn.rawgit.com/oliver-moran/jimp/v0.2.27/browser/lib/jimp.min.js');
 		Jimp.read(e.data.image).then(function(image) {
 			processImage(image, e.data.props, Jimp).getBase64(Jimp.AUTO, function(err, src) {
 				self.postMessage({ src, err });
@@ -11,5 +11,4 @@ module.exports = function worker(self) {
 			});
 		});
 	};
-}
-
+};
