17a1f2beb9367d773b979b68f5bf1065b4814dbb
Nitin <tulswani19@gmail.com>
2017-09-02 15:38:26 +0530

    resize modes

---
+++ src/utils/modes.js
@@ -0,0 +1,23 @@
+// Resize modes
+const RESIZE_MODES = {
+	neighbor: 'RESIZE_NEAREST_NEIGHBOR',
+	bilinear: 'RESIZE_BILINEAR',
+	bicubic: 'RESIZE_BICUBIC',
+	hermite: 'RESIZE_HERMITE',
+	bezier: 'RESIZE_BEZIER',
+};
+
+// Align modes (contain and cover)
+const ALIGN_MODES = {
+	horizontal_left: 'HORIZONTAL_ALIGN_LEFT',
+	horizontal_center: 'HORIZONTAL_ALIGN_CENTER',
+	horizontal_right: 'HORIZONTAL_ALIGN_RIGHT',
+	vertical_top: 'VERTICAL_ALIGN_TOP',
+	vertical_middle: 'VERTICAL_ALIGN_MIDDLE',
+	vertical_bottom: 'VERTICAL_ALIGN_BOTTOM',
+};
+
+module.exports = {
+	ALIGN_MODES,
+	RESIZE_MODES,
+};
