a8eabfd02ab86586e88bff2107caa757344ec0f7
Nitin <tulswani19@gmail.com>
2017-09-22 14:31:00 +0530

    SSR support

--- src/utils/build.js
+++ src/utils/build.js
@@ -1,6 +1,7 @@
 // Depends on the Jimp browser build, accessible via cdn
 // <script src="https://cdn.rawgit.com/oliver-moran/jimp/v0.2.27/browser/lib/jimp.min.js"></script>
+import root from 'window-or-global';
 
-let ROOT = typeof window.Jimp === 'function' && window.Jimp !== undefined ? window.Jimp : undefined;
+let ROOT = typeof root.Jimp === 'function' && root.Jimp !== undefined ? root.Jimp : undefined;
 
 export default ROOT;
