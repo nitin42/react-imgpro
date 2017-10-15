28a47c3cc5d79fb0536e72cebf5a2e4c25ed21c6
Nitin <tulswani19@gmail.com>
2017-10-15 16:37:57 +0530

    removed quality check test

--- __tests__/options.test.js
+++ __tests__/options.test.js
@@ -6,17 +6,10 @@ describe('image options', () => {
 	const image = 'http://365.unsplash.com/assets/paul-jarvis-9530891001e7f4ccfcef9f3d7a2afecd.jpg';
 	let output = null;
 
-	test('should resize the image height', () => {
+	test('sanity check', () => {
 		Jimp.read(image).then(image => {
       const output = processImage(image, { resize: { height: 300 } }, Jimp) 
 			expect(output.bitmap.height).toEqual(300);
 		});
   });
-  
-  test('should change image quality', () => {
-    Jimp.read(image).then(image => {
-      const output = processImage(image, { quality: 50 }, Jimp);
-      console.log(Object.keys(output.bitmap.data))
-		});
-  })
 });
